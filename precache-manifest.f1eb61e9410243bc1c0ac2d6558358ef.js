7c3eb56fcbcfd2d45e4312569e536e8ff0bbc702
Armando <jaimes087.uz@gmail.com>
2020-07-13 23:08:10 -0500

    Updates

---
+++ precache-manifest.f1eb61e9410243bc1c0ac2d6558358ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c824a2aae4f3c1424a3bf24320591e89",
+    "url": "/react-stopwatch/index.html"
+  },
+  {
+    "revision": "ba549d2d202053506ab8",
+    "url": "/react-stopwatch/static/css/main.dd655aa3.chunk.css"
+  },
+  {
+    "revision": "7b4aca7cd8aad2a5d1c4",
+    "url": "/react-stopwatch/static/js/2.2b8945b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-stopwatch/static/js/2.2b8945b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba549d2d202053506ab8",
+    "url": "/react-stopwatch/static/js/main.b8f23be3.chunk.js"
+  },
+  {
+    "revision": "73bf0c21e233a512bbca",
+    "url": "/react-stopwatch/static/js/runtime-main.821ccdb1.js"
+  }
+]);
